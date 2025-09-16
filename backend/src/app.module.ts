3aaf945cc7574b667022a2416722c6ec9c5645e6
b-sw <45360754+b-sw@users.noreply.github.com>
2025-09-16 21:13:05 +0200

    fix: add invitations module to root module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { AuthCoreModule } from './auth/core/auth-core.module';
 import { GithubAuthModule } from './auth/github/github-auth.module';
 import { GoogleAuthModule } from './auth/google/google-auth.module';
+import { InvitationCoreModule } from './invitation/core/invitation-core.module';
 import { ProjectCoreModule } from './project/core/project-core.module';
 import { getEnvConfig } from './shared/config/env-config';
 import { LogdashModule } from './shared/logdash/logdash.module';
@@ -15,6 +16,7 @@ import { UserCoreModule } from './user/core/user-core.module';
     MongooseModule.forRoot(getEnvConfig().mongo.url),
     UserCoreModule,
     ProjectCoreModule,
+    InvitationCoreModule,
     ScheduleModule.forRoot(),
     EventEmitterModule.forRoot(),
     AuthCoreModule,
