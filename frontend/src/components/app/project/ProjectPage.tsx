9d84239736f2dbc23e9726eb21c7f01e8fa447f9
ablaszkiewicz <kqmdjc8@gmail.com>
2025-09-20 09:19:32 +0200

    feat: wip

--- frontend/src/components/app/project/ProjectPage.tsx
+++ frontend/src/components/app/project/ProjectPage.tsx
@@ -1,14 +1,24 @@
 import { DesktopProjectView } from "@/components/app/project/desktop/DesktopProjectView";
 import { MobileProjectView } from "@/components/app/project/mobile/MobileProjectView";
-import { useParams } from "@tanstack/react-router";
-import { BindLogic } from "kea";
+import { useNavigate, useParams } from "@tanstack/react-router";
+import { BindLogic, useValues } from "kea";
 import { projectLogic } from "@/lib/logics/projectLogic";
 import { useEffect, useState } from "react";
+import { authLogic } from "@/lib/logics/authLogic";
 
 export function ProjectPage() {
+  const navigate = useNavigate();
+  const { isLoggedIn } = useValues(authLogic);
+
   const projectId = useParams({
     from: "/app/project/$projectId",
   });
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate({ to: "/app/login" });
+    }
+  }, []);
 
   return (
     <BindLogic logic={projectLogic} props={{ projectId: projectId.projectId }}>
