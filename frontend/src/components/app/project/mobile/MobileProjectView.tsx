a300a45ff31ec67bfa671848f71331f030460a30
ablaszkiewicz <kqmdjc8@gmail.com>
2025-09-21 13:01:32 +0200

    fix: mobile height fix

--- frontend/src/components/app/project/mobile/MobileProjectView.tsx
+++ frontend/src/components/app/project/mobile/MobileProjectView.tsx
@@ -2,7 +2,7 @@ import { MobileProjectTile } from "./MobileProjectTile";
 
 export function MobileProjectView() {
   return (
-    <div className="h-screen w-full overflow-hidden bg-background text-foreground">
+    <div className="h-screen-mobile w-full overflow-hidden bg-background text-foreground">
       <MobileProjectTile />
     </div>
   );
